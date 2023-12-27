2c2e6a256956709bb0122498d2d5e055e39ebbd1
jl682450 <66740724+jl682450@users.noreply.github.com>
2023-12-27 03:07:32 -0500

    pytest_src_egg add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Its a wineQT package",
+    author="joeltho",
+    packages=find_packages(),
+    license="MIT"
+)
